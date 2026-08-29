1d38555a57fbceda57e4fbf6b185a25c05f3aa77
Dilara Valitova <valitovadilara@icloud.com>
2026-08-29 15:35:24 +0300

    unused import was deleted

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.storage.user;
 
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
