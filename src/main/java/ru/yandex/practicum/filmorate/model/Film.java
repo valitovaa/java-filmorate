0a7cdc8cd1f4d36012881a67e44a14795be21fd9
Dilara Valitova <valitovadilara@icloud.com>
2026-08-29 04:48:54 +0300

    deleted unused import

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import lombok.Getter;
 
 import java.time.LocalDate;
 
