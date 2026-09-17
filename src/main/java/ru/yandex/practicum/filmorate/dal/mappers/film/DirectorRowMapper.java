031e550b66118b750ce67f74da0c975678b961b7
Markoskrasava <markbyckov8@gmail.com>
2026-09-17 13:05:30 +0300

    Добавить аннотацию для маппера

--- src/main/java/ru/yandex/practicum/filmorate/dal/mappers/film/DirectorRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/mappers/film/DirectorRowMapper.java
@@ -1,11 +1,13 @@
 package ru.yandex.practicum.filmorate.dal.mappers.film;
 
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.film.Director;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@Component
 public class DirectorRowMapper implements RowMapper<Director>  {
     @Override
     public Director mapRow(ResultSet rs, int rowNum) throws SQLException {
