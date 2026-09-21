bc197102147c802c5481f3c5af29908b97598b25
Markoskrasava <markbyckov8@gmail.com>
2026-09-21 19:12:28 +0300

    Исправить выброс ошибок

--- src/main/java/ru/yandex/practicum/filmorate/dal/repositories/user/UserRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/repositories/user/UserRepository.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.dal.repositories.user;
 
-import jakarta.validation.ValidationException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
