49aadf263b01794ed7b8ebe258902a1d03ed9293
Dilara Valitova <valitovadilara@icloud.com>
2026-09-23 12:13:29 +0500

    fixed EventService

--- src/main/java/ru/yandex/practicum/filmorate/service/EventService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/EventService.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.service;
 
-import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.feed.Event;
@@ -10,11 +11,18 @@ import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class EventService {
 
     private final EventStorage eventStorage;
     private final UserStorage userStorage;
+
+    public EventService(
+            EventStorage eventStorage,
+            @Qualifier("userDbStorage") UserStorage userStorage
+    ) {
+        this.eventStorage = eventStorage;
+        this.userStorage = userStorage;
+    }
 
     public List<Event> getEvents(Long userId) {
         userStorage.findUserById(userId).orElseThrow(() -> new NotFoundException("Пользователь не найден"));
